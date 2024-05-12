7b7d90028923a71576d55020158d34b4c1389859
Jin Geonwoo <jinkonu99@gmail.com>
2024-05-12 15:48:44 +0900

    [Feat] 프로젝트 셋업(#1)

---
+++ planfit/src/main/java/com/planfit/server/PlanfitApplication.java
@@ -0,0 +1,13 @@
+package com.planfit.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanfitApplication.class, args);
+	}
+
+}
