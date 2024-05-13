70dc8c4839b6679e551a3b704fd77a408ff55804
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-05-13 18:08:49 +0900

    [Feat] ApiResponseUtil 구현(#3)

---
+++ planfit/src/main/java/com/planfit/server/common/ApiResponseUtil.java
@@ -0,0 +1,20 @@
+package com.planfit.server.common;
+
+import org.springframework.http.ResponseEntity;
+
+public interface ApiResponseUtil {
+    static ResponseEntity<BaseResponse<?>> success(SuccessMessage successMessage) {
+        return ResponseEntity.status(successMessage.getHttpStatus())
+                .body(BaseResponse.of(successMessage));
+    }
+
+    static <T> ResponseEntity<BaseResponse<?>> success(SuccessMessage successMessage, T data) {
+        return ResponseEntity.status(successMessage.getHttpStatus())
+                .body(BaseResponse.of(successMessage, data));
+    }
+
+    static ResponseEntity<BaseResponse<?>> failure(ErrorMessage errorMessage) {
+        return ResponseEntity.status(errorMessage.getHttpStatus())
+                .body(BaseResponse.of(errorMessage));
+    }
+}
