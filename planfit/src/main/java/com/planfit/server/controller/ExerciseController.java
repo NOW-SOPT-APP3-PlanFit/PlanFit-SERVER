567cf41c722b2dd84a61fc071f7b3b996ee4b7db
chaewonkim <kimlove9182@gmail.com>
2024-05-14 22:23:38 +0900

    [Feat] 운동 리스트 조회 API 구현 - 에러 처리(#9)

--- planfit/src/main/java/com/planfit/server/controller/ExerciseController.java
+++ planfit/src/main/java/com/planfit/server/controller/ExerciseController.java
@@ -2,6 +2,7 @@ package com.planfit.server.controller;
 
 import com.planfit.server.common.ApiResponseUtil;
 import com.planfit.server.common.BaseResponse;
+import com.planfit.server.common.message.ErrorMessage;
 import com.planfit.server.common.message.SuccessMessage;
 import com.planfit.server.dto.request.ExerciseFindAllDto;
 import com.planfit.server.service.ExerciseService;
@@ -25,7 +26,11 @@ public class ExerciseController {
 
     //운동 리스트 조회
     @GetMapping("/exercises")
-    public ResponseEntity<BaseResponse<?>> exercisesList(@RequestHeader Long userId) {
+    public ResponseEntity<BaseResponse<?>> exercisesList(@RequestHeader(required = false) Long userId) {
+        if (userId == null || userId != 1) {
+            return ApiResponseUtil.failure(ErrorMessage.EXERCISE_BAD_REQUEST);
+        }
+
         List<ExerciseFindAllDto> exercises = exerciseService.findExercises(userId);
         Map<String, Object> responseData = new HashMap<>();
         responseData.put("exercises", exercises);
