0f4a1beb2cee30c16d7ece2b92228b7ae5edb676
chaewonkim <kimlove9182@gmail.com>
2024-05-16 03:37:27 +0900

    [Chore] exercise 서비스에서 찾아오는 메서드 추가(#13)

--- planfit/src/main/java/com/planfit/server/service/ExerciseService.java
+++ planfit/src/main/java/com/planfit/server/service/ExerciseService.java
@@ -2,10 +2,12 @@ package com.planfit.server.service;
 
 import com.planfit.server.common.exception.NotFoundException;
 import com.planfit.server.common.message.ErrorMessage;
+import com.planfit.server.domain.Exercise;
 import com.planfit.server.domain.Routine;
 import com.planfit.server.domain.User;
 import com.planfit.server.dto.request.ExerciseReorderRequest;
 import com.planfit.server.dto.response.ExerciseGetAllResponse;
+import com.planfit.server.repository.ExerciseRepository;
 import com.planfit.server.repository.RoutineRepository;
 import com.planfit.server.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ public class ExerciseService {
 
     private final UserRepository userRepository;
     private final RoutineRepository routineRepository;
+    private final ExerciseRepository exerciseRepository;
 
     //운동 리스트 조회
     public ExerciseGetAllResponse findExercises(Long userId) {
@@ -34,6 +37,10 @@ public class ExerciseService {
     public void reorderExercises(Long userId, List<ExerciseReorderRequest> exercises) {
         User user = userRepository.findById(userId).orElseThrow();
 
+        for (ExerciseReorderRequest exercise : exercises) {
+            getExercise(exercise.id());
+        }
+
         //모든 운동의 sequence를 임시값으로 초기화
         List<Routine> routines = routineRepository.findAllByUserId(userId);
         int tempSequence = -1000;
@@ -45,10 +52,15 @@ public class ExerciseService {
         //새로운 순서로 업데이트
         for (ExerciseReorderRequest exercise : exercises) {
             Routine routine = routineRepository.findByExerciseIdAndUserId(exercise.id(), user.getId())
-                    .orElseThrow(() -> new NotFoundException(ErrorMessage.EXERCISE_NOT_FOUND));
+                    .orElseThrow(() -> new NotFoundException(ErrorMessage.ROUTINE_NOT_FOUND));
             routine.updateSequence(exercise.index());
             routineRepository.save(routine);
         }
 
+    }
+
+    public Exercise getExercise(Long exerciseId) {
+        return exerciseRepository.findById(exerciseId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.EXERCISE_NOT_FOUND));
     }
 }
