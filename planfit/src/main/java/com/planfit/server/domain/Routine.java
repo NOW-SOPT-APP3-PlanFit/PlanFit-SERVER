7f8303ee54523bc854393267f978c5735b0745fc
chaewonkim <kimlove9182@gmail.com>
2024-05-16 18:21:06 +0900

    [Feat] JPA 엔티티 수정(#16)

--- planfit/src/main/java/com/planfit/server/domain/Routine.java
+++ planfit/src/main/java/com/planfit/server/domain/Routine.java
@@ -3,6 +3,9 @@ package com.planfit.server.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
@@ -27,11 +30,6 @@ public class Routine {
     @ManyToOne(fetch = FetchType.LAZY)
     private Exercise exercise;
 
-    public void like() {
-        this.isLike = true;
-    }
-
-    public void unlike() {
-        this.isLike = false;
-    }
+    @OneToMany(mappedBy = "routine")
+    private List<Set> sets = new ArrayList<>();
 }
